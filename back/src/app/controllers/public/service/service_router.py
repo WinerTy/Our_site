d24145e393dcec58c224f46a8106b6171d1b0170
WinerTy <nikitokmay@gmail.com>
2024-09-16 12:33:12 +0500

    No quest

--- back/src/app/controllers/public/service/service_router.py
+++ back/src/app/controllers/public/service/service_router.py
@@ -7,19 +7,6 @@ from ....repositories.service_repo import service_repository
 router = APIRouter(prefix="/service", tags=["service"])
 
 
-# @router.post("/", response_model=ServiceResponce)
-# async def create_bib(
-#     bib_data: BibCreate,
-#     user: User = Depends(current_user),
-#     session: AsyncSession = Depends(get_session),
-# ) -> BibResponse:
-#     data = bib_data.dict()
-#     if user:
-#         data["user_id"] = user.id
-#     bib = await bib_repository.create(data)
-#     return bib
-
-
 @router.post("/", response_model=ServiceResponce)
 async def create_service(
     service_data: ServiceCreate, session: AsyncSession = Depends(get_session)
